7bb2818a78734457b98f5abcb5702eee5d162deb
RickyJohan <73305378+RickyJohan@users.noreply.github.com>
2020-12-04 23:34:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const checkbox = document.getElementById('checkbox');
+const container = document.querySelector('.container');
+const label = document.querySelector('.label');
+const header = document.querySelector ('header');
+const ball = document.querySelector('.ball');
+const hamburgerButton = document.querySelectorAll ('.line');
+const text = document.querySelector ('#body');
+
+checkbox.addEventListener ('click', () =>{
+  //change the website theme
+  container.classList.toggle('dark');
+  //change button theme
+  label.classList.toggle('dark');
+  //change header theme
+  header.classList.toggle('dark-2');
+  //change button theme
+  ball.classList.toggle('dark-3');
+  //change hamburger button line theme
+  for (let i = 0; i < hamburgerButton.length; i++){
+    hamburgerButton[i].classList.toggle('dark-4');
+  };
+  //change the text color to white
+  text.classList.toggle('white');
+});
+
+const menuIcon = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector (".nav-bar");
+
+menuIcon.addEventListener("click", () => {
+  //open navbar
+  navbar.classList.toggle("change");
+  //make hamburger button sliding
+  menuIcon.classList.toggle("slide");
+//make body smaller
+  text.classList.toggle('size');
+});
